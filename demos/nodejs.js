56aea72317516eb96bf4ae0aabf3c86227045425
Dark25 <105049633+Dark-TwentyFive@users.noreply.github.com>
2023-05-29 23:24:08 +1000

    Create nodejs.js

---
+++ demos/nodejs.js
@@ -0,0 +1,13 @@
+const fetch = require('node-fetch');
+
+const apiKey = 'your_api_key_here'; // Replace with your actual NeuraNET API key
+const message = encodeURIComponent('Hello NeuraNET!');
+
+fetch(`https://neuranet-api.dark9015.repl.co/api/${apiKey}/${message}`, {
+    method: 'GET'
+})
+.then(response => response.text())
+.then(data => console.log(data))
+.catch((error) => {
+    console.error('Error:', error);
+});
